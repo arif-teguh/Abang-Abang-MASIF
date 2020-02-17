6efbb12d13d2d239407e39f3d2615ba734b51aec
Azhar Rais <dev@azharaiz.com>
2020-02-17 13:56:30 +0700

    [CHORES] Add sonar-scanner

--- masifapp/settings.py
+++ masifapp/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2=t-_dm)m_1&_fw&e^trsauh_zkjagkhwe#-bd^ouv6fd35^2v'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "masif.herokuapp.com"]
 
