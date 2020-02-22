6b5df38ad70f8f7bbe607d400313356f21b36df1
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-02-22 22:35:42 +0700

    [RED] Test login admin url exist

--- admin/tests.py
+++ admin/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
+
+class AdminUnitTest(TestCase):
+    def test_admin_url_exist(self):
+        response = Client().get('/admin')
+        self.assertEqual(response.status_code, 200)
 
 # Create your tests here.
