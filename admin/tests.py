4ecd7000d8bade6eb0eb22b4e9294701dbfc6904
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-02-22 23:19:33 +0700

    [RED] Added admin login page test (function used, template used, page title)

--- admin/tests.py
+++ admin/tests.py
@@ -1,9 +1,27 @@
+from django.http import HttpRequest
 from django.test import TestCase, Client
+from django.urls import resolve
+
+from . import views
 
 
 class AdminUnitTest(TestCase):
     def test_admin_url_exist(self):
-        response = Client().get('/admin')
+        response = Client().get('/admin/')
         self.assertEqual(response.status_code, 200)
+
+    def test_admin_login_using_admin_login_function(self):
+        function_used = resolve('/admin/')
+        self.assertEqual(function_used.func, views.admin_login)
+
+    def test_page_title_admin_login(self):
+        request = HttpRequest()
+        response = views.admin_login(request)
+        html_response = response.content.decode('utf8')
+        self.assertIn('<title>Admin Login</title>', html_response)
+
+    def test_using_admin_login_html(self):
+        response = Client().get('/admin/')
+        self.assertTemplateUsed(response, 'admin_login.html')
 
 # Create your tests here.
