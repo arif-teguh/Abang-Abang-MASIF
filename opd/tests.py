bfbc04484f2a01e455c8bd1b77fc261ef4b34077
arif teguh wangi <arifteguhwangi@gmail.com>
2020-02-22 20:12:02 +0700

    [RED] membuat test urls dan template  login opd

--- opd/tests.py
+++ opd/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
+from .views import *
+from django.http import HttpRequest
 
-# Create your tests here.
+class opdPage(TestCase):
+	def test_url_exist(self):
+		response = self.client.get('/opd/login')
+		self.assertEqual(response.status_code,200)
+		
+	def test_main_template(self):
+		response = self.client.get('/opd/login')
+		self.assertTemplateUsed(response,'opdLogin.html')
+				
+
+
+
+
+  
+		
+		
+	
+		
