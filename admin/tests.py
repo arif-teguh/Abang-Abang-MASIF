afb7dd2164b40782c04cbc43d21e374270b4cc8b
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-02-23 00:14:20 +0700

    [RED] Form Button Test

--- admin/tests.py
+++ admin/tests.py
@@ -28,4 +28,10 @@ class AdminUnitTest(TestCase):
         response = Client().get('/admin/login/')
         self.assertTemplateUsed(response, 'admin/admin_login.html')
 
+    def test_submit_button_exist(self):
+        request = HttpRequest()
+        response = views.index(request)
+        html_response = response.content.decode('utf8')
+        self.assertIn('<button type="submit"', html_response)
+
 # Create your tests here.
