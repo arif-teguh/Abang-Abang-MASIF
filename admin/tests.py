50f8f094233e2caee81b60cf30d9a7bbcc90e18d
Azhar Rais <dev@azharaiz.com>
2020-02-24 03:45:55 +0700

    [RED] add verification email test

--- admin/tests.py
+++ admin/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.core import mail
+from . import mailing
 
-# Create your tests here.
+
+class EmailTest(TestCase):
+    verification_url = 'masif.com/abcdefg'
+    recipient_email = 'test@test.com'
+
+    def test_send_email_sent(self):
+        mailing.send_verification_email(self.verification_url, self.recipient_email)
+        self.assertEqual(len(mail.outbox), 1)
+        mail.outbox = []
+
+    def test_email_subject(self):
+        mailing.send_verification_email(self.verification_url, self.recipient_email)
+        self.assertEqual(mail.outbox[0].subject, 'Verifikasi OPD')
+        mail.outbox = []
+
+    def test_email_body(self):
+        mailing.send_verification_email(self.verification_url, self.recipient_email)
+        self.assertIn(self.verification_url, mail.outbox[0].body)
+        mail.outbox = []
+
+    def test_invalid_email(self):
+        with self.assertRaises(ValueError):
+            mailing.send_verification_email(self.verification_url, "test.com")
