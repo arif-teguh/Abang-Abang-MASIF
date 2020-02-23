1c7f75f3cf7e5f79530a64457ffb08c53b182d0d
Azhar Rais <dev@azharaiz.com>
2020-02-24 05:22:56 +0700

    [GREEN] create send_verification_email methods on admin/mailing.py

---
+++ admin/mailing.py
@@ -0,0 +1,17 @@
+from django.core import mail
+from django.test import TestCase
+
+import re
+
+
+def send_verification_email(verification_url, recipient_email):
+    email_validator = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+    if(re.search(email_validator, recipient_email)):
+        mail.send_mail(
+            'Verifikasi OPD',
+            'Berikut link untuk verifikasi email untuk akun OPD: ' + verification_url,
+            'admin@masif.com',
+            [recipient_email]
+        )
+    else:
+        raise ValueError("Email not valid")
