bbf1f259ce0597ea565bcb3d531b37b3bf7e59fe
arif teguh wangi <arifteguhwangi@gmail.com>
2020-02-23 15:55:15 +0700

    [GREEN] Implement opd/login/ and opd/ url. Implement templates. Added template folder.

--- opd/tests.py
+++ opd/tests.py
@@ -5,12 +5,12 @@ from .views import *
 from django.http import HttpRequest
 
 class opdPage(TestCase):
-	def test_url_exist(self):
-		response = self.client.get('/opd/login')
+	def test_opd_login_url_exist(self):
+		response = self.client.get('/opd/login/')
 		self.assertEqual(response.status_code,200)
 		
-	def test_main_template(self):
-		response = self.client.get('/opd/login')
+	def test_opd_login_template(self):
+		response = self.client.get('/opd/login/')
 		self.assertTemplateUsed(response,'opdLogin.html')
 				
 
