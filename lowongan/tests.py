13a367ac1d7248929b973bd343f125eefd3c1271
feril68 <muhammad.feril@ui.ac.id>
2020-02-25 11:53:21 +0700

    [REFACTOR] memperbaiki code smell yaitu pengulangan konstanta

--- lowongan/tests.py
+++ lowongan/tests.py
@@ -4,15 +4,16 @@ from django.urls import resolve
 
 from . import views
 
+url_form_lowongan = '/lowongan/opd/form/'
 class LowonganFormTest(TestCase):
     def test_form_lowongan_url_exist(self):
-        response = Client().get('/lowongan/opd/form/')
+        response = Client().get(url_form_lowongan)
         self.assertEqual(response.status_code, 200)
 
     def test_form_lowongan_using_form_lowongan_function(self):
-        function_used = resolve('/lowongan/opd/form/')
+        function_used = resolve(url_form_lowongan)
         self.assertEqual(function_used.func, views.show_form_lowongan)
 
     def test_form_lowongan_using_template_form_lowongan(self):
-        response = Client().get('/lowongan/opd/form/')
+        response = Client().get(url_form_lowongan)
         self.assertTemplateUsed(response, 'lowongan/form_lowongan.html')
