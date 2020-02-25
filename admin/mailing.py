eb78ad4531410367d15a0f1a445f901765744acb
Azhar Rais <dev@azharaiz.com>
2020-02-25 19:08:19 +0700

    [REFACTOR] Do some cleanup for pylint warning

--- admin/mailing.py
+++ admin/mailing.py
@@ -1,15 +1,16 @@
-from django.core import mail
-from django.test import TestCase
-
 import re
+
+from django.core import mail
+
 
 
 def send_verification_email(verification_url, recipient_email):
     email_validator = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-    if(re.search(email_validator, recipient_email)):
+    if re.search(email_validator, recipient_email):
         mail.send_mail(
             'Verifikasi OPD',
-            'Berikut link untuk verifikasi email untuk akun OPD: ' + verification_url,
+            'Berikut link untuk verifikasi email untuk akun OPD: '
+            + verification_url,
             'admin@masif.com',
             [recipient_email]
         )
