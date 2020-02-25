ad92667b0758992e3256341767f2ac7e3051869c
feril68 <muhammad.feril@ui.ac.id>
2020-02-25 11:07:28 +0700

    [GREEN] implementasi template yang dirender oleh show_form_lowongan

--- lowongan/views.py
+++ lowongan/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 def show_form_lowongan(request):
-    return HttpResponse("<h1>test<h1>")
+    return render(request, 'lowongan/form_lowongan.html')
