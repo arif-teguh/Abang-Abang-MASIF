b8ad7ce6f5921f52f914b539517701adbe174869
feril68 <muhammad.feril@ui.ac.id>
2020-02-25 10:57:19 +0700

    [RED] test untuk pengecekan html yang dirender show_form_lowongan

--- lowongan/tests.py
+++ lowongan/tests.py
@@ -12,3 +12,7 @@ class LowonganFormTest(TestCase):
     def test_form_lowongan_using_form_lowongan_function(self):
         function_used = resolve('/lowongan/opd/form/')
         self.assertEqual(function_used.func, views.show_form_lowongan)
+
+    def test_form_lowongan_using_template_form_lowongan(self):
+        response = Client().get('/lowongan/opd/form/')
+        self.assertTemplateUsed(response, 'lowongan/form_lowongan.html')
