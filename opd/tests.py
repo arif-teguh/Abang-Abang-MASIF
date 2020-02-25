bfb2cf36563fab40fdbd800928d97917fcd0189b
arif teguh wangi <arifteguhwangi@gmail.com>
2020-02-25 15:30:08 +0700

    [REFACTOR] memperbaiki code smell yaitu penamaan class name pada test

--- opd/tests.py
+++ opd/tests.py
@@ -4,7 +4,7 @@ from django.urls import resolve
 from .views import *
 from django.http import HttpRequest
 
-class opdPage(TestCase):
+class opd_page(TestCase):
 	def test_opd_login_url_exist(self):
 		response = self.client.get('/opd/login/')
 		self.assertEqual(response.status_code,200)
