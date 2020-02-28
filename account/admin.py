48b40a0737b46ea6fd17d6dab3ba7d1e65e56b79
Kevin Raikhan Zain <kevin.raikhan@ui.ac.id>
2020-02-29 01:25:04 +0700

    [CHORES] Added account models to admin.py so that superuser can manage accounts

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from account.models import Account, OpdProfile, AdminProfile
+
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(OpdProfile)
+admin.site.register(AdminProfile)
