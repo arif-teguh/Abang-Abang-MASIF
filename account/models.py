cb2d5977df2cd5b3742521e050e130559e5e33be
Kevin Raikhan Zain <kevin.raikhan@ui.ac.id>
2020-02-29 01:06:57 +0700

    [GREEN] Implemented temporary custom accounts model(User, superuser, opd, admin)

--- account/models.py
+++ account/models.py
@@ -1,3 +1,85 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have email address")
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        if not email:
+            raise ValueError("Users must have email address")
+
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password)
+        user.name = "SuperUser"
+        user.phone = "000"
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    profile_picture = models.FileField(
+        default="static/default-profile.png",
+        max_length=500)
+    name = models.CharField(max_length=128)
+    email = models.EmailField(
+        verbose_name="email", max_length=254, unique=True)
+    phone = models.CharField(max_length=26)
+    date_joined = models.DateTimeField(
+        verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_opd = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = AccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class OpdProfile(models.Model):
+    user = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE,
+        primary_key=True, null=False, blank="False"
+    )
+    # Temporary Attribute
+    unique_opd_attribute = models.CharField(max_length=60)
+
+    def __str__(self):
+        return "<OPD Profile> {}".format(self.unique_opd_attribute)
+
+
+class AdminProfile(models.Model):
+    user = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE,
+        primary_key=True, null=False, blank="False"
+    )
+    # Temporary Attribute
+    unique_admin_attribute = models.CharField(max_length=60)
+
+    def __str__(self):
+        return "<ADMIN Profile> {}".format(self.unique_admin_attribute)
