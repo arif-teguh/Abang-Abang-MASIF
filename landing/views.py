679e61488ad38559ef027818100bd1e613ec664d
khrisnaa3 <stefanuskhrisnaproject@gmail.com>
2020-02-29 22:25:24 +0700

    [CHORES] Landing Page Done (not yet responsive for mobile devices)

--- landing/views.py
+++ landing/views.py
@@ -4,7 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def landing(request):
-    try:
-        return render(request, 'landing_page.html')
-    except:
-        print("There is an error")
+    return render(request, 'landing_page.html')
