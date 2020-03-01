199e6ba214b0f232ab1a14fe2113b5be32b893d8
arif teguh wangi <arifteguhwangi@gmail.com>
2020-03-01 14:36:07 +0700

    [RED] Created test untuk opd login dan button html test

--- opd/tests.py
+++ opd/tests.py
@@ -1,24 +1,35 @@
-from django.test import TestCase
-from django.test import Client
+from django.test import TestCase, Client
 from django.urls import resolve
-from .views import *
 from django.http import HttpRequest
+from . import views
+from account.models import Account
+
 
 class OpdUnitTest(TestCase):
-	def test_opd_login_url_exist(self):
-		response = self.client.get('/opd/login/')
-		self.assertEqual(response.status_code,200)
-		
-	def test_opd_login_template(self):
-		response = self.client.get('/opd/login/')
-		self.assertTemplateUsed(response,'opdLogin.html')
-				
+    def test_page_title_opd_login(self):
+        request = HttpRequest()
+        response = views.opd_login(request)
+        html_response = response.content.decode('utf8')
+        self.assertIn('<title>OPD Login</title>', html_response)
+
+    def test_opd_login_template(self):
+        response = self.client.get('/opd/login/')
+        self.assertTemplateUsed(response,'opd_login.html')
+        
+
+    def test_submit_button_exist(self):
+        request = HttpRequest()
+        response = views.opd_login(request)
+        html_response = response.content.decode('utf8')
+        self.assertIn('<button type="submit"', html_response)
 
 
+    def test_opd_login_page_is_set_up_as_expected(self):
+        response = Client().get('/opd/login/')
+        self.assertEqual(200, response.status_code)
+        form = response.context['form']
+        self.assertTrue(isinstance(form, OpdAuthenticationForm), type(form).__mro__)
 
-
-  
-		
-		
-	
-		
+    def test_displays_opd_login_form(self):
+        response = Client().get('/opd/login/')
+        self.assertIsInstance(response.context["form"], OpdAuthenticationForm)
