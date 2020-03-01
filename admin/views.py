3bc94cae74ec99ea8f216c519c610cba71782e2e
Kevin Raikhan Zain <kevin.raikhan@ui.ac.id>
2020-03-01 20:06:51 +0700

    [GREEN] Implemented get all opd query function

--- admin/views.py
+++ admin/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+from account.models import Account
+
+
 def get_all_opd():
-    pass
+    return list(Account.objects.filter(is_opd=True, is_admin=False, is_staff=False, is_superuser=False, is_user=False))
+    # return list(Account.objects.all())
 
 def user_is_admin(request):
     return request.user.is_authenticated and request.user.is_admin and not request.user.is_opd and not request.user.is_user
