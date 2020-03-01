d6d31ddb088f5d65c68754ad5ad5a97830624f1e
arif teguh wangi <arifteguhwangi@gmail.com>
2020-03-01 14:47:52 +0700

    [GREEN] Implement for opd page restriction

--- opd/urls.py
+++ opd/urls.py
@@ -6,5 +6,6 @@ from opd.opd_login_form import OpdAuthenticationForm
 from . import views
 
 urlpatterns = [
+    path('', views.opd_index, name='opd'),
     path('login/', LoginView.as_view(template_name='opd_login.html',form_class=OpdAuthenticationForm,),name='opd_login'),
 ]
--- opd/views.py
+++ opd/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def opd_login(request):
     return render(request,'opd_login.html')
+
+def opd_index(request):
+    if request.user.is_authenticated and not request.user.is_admin and request.user.is_opd and not request.user.is_user:
+        return HttpResponse("<h1>opd PAGE, Under Construction</h1>")
+    else:
+        return redirect('/account-redirector')
