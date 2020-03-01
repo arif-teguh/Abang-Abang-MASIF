7d06fd17af52c08244a362233c034da0f174ebfe
arif teguh wangi <arifteguhwangi@gmail.com>
2020-03-01 22:24:15 +0700

    [REFACTOR] memperbagus html dan menambah dummy url untuk check

--- opd/views.py
+++ opd/views.py
@@ -18,3 +18,7 @@ def opd_lowongan(request):
 def opd_detail_lowongan(request,id_lowongan):
     lowongan = Lowongan.objects.get(id = id_lowongan)
     return render(request,'opd_detail_lowongan.html' , {'lowongan': lowongan})
+
+def opd_detail_dummy(request):
+    lowongan = {'judul' : 'judul' , 'deskripsi' : 'deskripsi', 'penyedia' : 'penyedia' , 'requirement' :'requirement' ,'durasi_magang' : 'durasi_magang','jumlah_tersedia':'jumlah_tersedia'}
+    return render(request,'opd_detail_lowongan.html' , {'lowongan': lowongan})
