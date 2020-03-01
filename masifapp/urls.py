e4344987e268a2239fb2e47215cfd6a9d55b6e49
Kevin Raikhan Zain <kevin.raikhan@ui.ac.id>
2020-03-01 16:58:19 +0700

    [REFACTOR] Added superuser url to open djangoadmin

--- masifapp/urls.py
+++ masifapp/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', include('admin.urls')),
     path('', include('hello.urls')),
     path('account-redirector', include('account_redirector.urls')),
+    path('superuser/', admin.site.urls),
 
 ]
