2dd176dd689b55a4d39ca89d79fa62ffa39c4152
Kevin Raikhan Zain <kevin.raikhan@ui.ac.id>
2020-03-02 15:31:06 +0700

    [RED] Test not authenticated user that access admin now redirected to admin/login/

--- admin/tests.py
+++ admin/tests.py
@@ -10,9 +10,12 @@ from admin.admin_login_form import AdminAuthenticationForm
 class AdminUnitTest(TestCase):
     def setUp(self):
         # Setup run before every test method.
+        self.client = Client()
         self.request = HttpRequest()
         Account.objects.create_user(email='test@mail.com', password='12345678')
         self.created_mock_user = Account.objects.all()[0]
+        self.client.login(username='test@mail.com', password='12345678')
+        self.request.user = self.created_mock_user
 
     def tearDown(self):
         # Clean up run after every test method.
@@ -47,11 +50,9 @@ class AdminUnitTest(TestCase):
     def test_admin_page_not_authenticated(self):
         response = Client().get('/admin/')
         self.assertEqual(response.status_code, 302)
-        self.assertEqual('/account-redirector', response.url)
+        self.assertEqual('admin/login/', response.url)
 
     def test_admin_access_admin_page(self):
-        created_mock_user = Account.objects.all()[0]
-        self.request.user = created_mock_user
         self.request.user.is_admin = True
         self.request.user.is_opd = False
         self.request.user.is_user = False
@@ -60,8 +61,6 @@ class AdminUnitTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_opd_access_admin_page(self):
-        created_mock_user = Account.objects.all()[0]
-        self.request.user = created_mock_user
         self.request.user.is_admin = False
         self.request.user.is_opd = True
         self.request.user.is_user = False
@@ -71,8 +70,6 @@ class AdminUnitTest(TestCase):
         self.assertEqual('/account-redirector', response.url)
 
     def test_user_access_admin_page(self):
-        created_mock_user = Account.objects.all()[0]
-        self.request.user = created_mock_user
         self.request.user.is_admin = False
         self.request.user.is_opd = False
         self.request.user.is_user = True
@@ -81,22 +78,42 @@ class AdminUnitTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual('/account-redirector', response.url)
 
+    def test_admin_access_admin_page_through_url(self):
+        self.created_mock_user.is_admin = True
+        self.created_mock_user.save()
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_opd_access_admin_page_through_url(self):
+        self.created_mock_user.is_admin = False
+        self.created_mock_user.is_opd = True
+        self.created_mock_user.save()
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/account-redirector')
+
+    def test_user_access_admin_page_through_url(self):
+        self.created_mock_user.is_admin = False
+        self.created_mock_user.is_user = True
+        self.created_mock_user.save()
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/account-redirector')
+
     def test_using_admin_index_func(self):
         found = resolve('/admin/')
         self.assertEqual(found.func, views.admin_index)
 
-    def test_admin_tambahkan_opd_not_authenticated_redirect_to_home(self):
+    def test_admin_list_opd_not_authenticated_redirect_to_admin_login_page(self):
         response = Client().get('/admin/listopd/')
         self.assertEqual(response.status_code, 302)
-        self.assertEqual('/account-redirector', response.url)
+        self.assertEqual('admin/login/', response.url)
 
     def test_using_admin_list_opd_func(self):
         found = resolve('/admin/listopd/')
         self.assertEqual(found.func, views.admin_list_opd)
 
     def test_admin_access_admin_opd_list_page(self):
-        created_mock_user = Account.objects.all()[0]
-        self.request.user = created_mock_user
         self.request.user.is_admin = True
         self.request.user.is_opd = False
         self.request.user.is_user = False
@@ -105,8 +122,6 @@ class AdminUnitTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_opd_access_admin_opd_list_page(self):
-        created_mock_user = Account.objects.all()[0]
-        self.request.user = created_mock_user
         self.request.user.is_admin = False
         self.request.user.is_opd = True
         self.request.user.is_user = False
@@ -116,8 +131,6 @@ class AdminUnitTest(TestCase):
         self.assertEqual('/account-redirector', response.url)
 
     def test_user_access_admin_opd_list_page(self):
-        created_mock_user = Account.objects.all()[0]
-        self.request.user = created_mock_user
         self.request.user.is_admin = False
         self.request.user.is_opd = False
         self.request.user.is_user = True
@@ -127,15 +140,14 @@ class AdminUnitTest(TestCase):
         self.assertEqual('/account-redirector', response.url)
 
     def test_function_get_all_opd_database_has_opd(self):
-        created_mock_user = Account.objects.all()[0]
-        created_mock_user.is_opd = True
-        created_mock_user.is_superuser = False
-        created_mock_user.is_admin = False
-        created_mock_user.is_user = False
-        created_mock_user.is_staff = False
-        created_mock_user.save()
+        self.created_mock_user.is_opd = True
+        self.created_mock_user.is_superuser = False
+        self.created_mock_user.is_admin = False
+        self.created_mock_user.is_user = False
+        self.created_mock_user.is_staff = False
+        self.created_mock_user.save()
         all_opd = views.get_all_opd()
-        self.assertEqual([created_mock_user], all_opd)
+        self.assertEqual([self.created_mock_user], all_opd)
 
     def test_function_get_all_opd_database_empty(self):
         all_opd = views.get_all_opd()
