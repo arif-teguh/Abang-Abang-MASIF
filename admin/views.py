6c3e751c9466583adcab961bb305e8454aa4137b
Kevin Raikhan Zain <kevin.raikhan@ui.ac.id>
2020-03-02 17:13:18 +0700

    [GREEN] Implemented not authenticated user will be redirected to admin login page when accessing admin page

--- admin/views.py
+++ admin/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from account.models import Account
@@ -6,27 +5,26 @@ from account.models import Account
 
 def get_all_opd():
     return list(Account.objects.filter(is_opd=True, is_admin=False, is_staff=False, is_superuser=False, is_user=False))
-    # return list(Account.objects.all())
+
 
 def user_is_admin(request):
     return request.user.is_authenticated and request.user.is_admin and not request.user.is_opd and not request.user.is_user
+
 
 def admin_login(request):
     return render(request, 'admin/admin_login.html')
 
 
 def admin_index(request):
-    if not request.user.is_authenticated:
-        return redirect('/admin/login/')
-    elif user_is_admin(request):
+    if user_is_admin(request):
         return render(request, 'admin/admin_index.html')
     else:
-        return redirect('/account-redirector')
-    
-def admin_list_opd(request):
-    if not request.user.is_authenticated:
         return redirect('/admin/login/')
-    elif user_is_admin(request):
-        return render(request, 'admin/admin_list_opd.html', {'list_opd' : get_all_opd()})
+
+
+def admin_list_opd(request):
+    if user_is_admin(request):
+        return render(request, 'admin/admin_list_opd.html', {'list_opd': get_all_opd()})
+
     else:
-        return redirect('/account-redirector')
+        return redirect('/admin/login/')
