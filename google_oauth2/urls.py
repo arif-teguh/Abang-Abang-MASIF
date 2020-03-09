17eef8ec710a4d151c894a6b126bcf2d7ba5d284
khrisnaa3 <stefanuskhrisnaproject@gmail.com>
2020-03-09 23:58:38 +0700

    [GREEN] Implement test case for login google-oauth2

--- google_oauth2/urls.py
+++ google_oauth2/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     # path('google-oauth2/', include('social_django.urls', namespace='sosial')),
-
-    path('', views.page_test)
+    path('auth/', include('social_django.urls', namespace='social')),
+    path('', views.page_test),
+    path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL},
+         name='logout'),
 ]
