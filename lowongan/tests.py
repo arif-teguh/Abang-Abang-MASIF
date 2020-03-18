8c5aacb93f9aeaebb8c798c2a4e41bf0ebf8e8e5
feril68 <muhammad.feril@ui.ac.id>
2020-03-18 10:39:15 +0700

    [RED] Membuat test untuk init pada form dan membuat choice option yang dinamis

--- lowongan/tests.py
+++ lowongan/tests.py
@@ -6,8 +6,11 @@ from account.models import Account, OpdProfile
 from .apps import LowonganConfig
 from .models import Lowongan
 from . import views
+from .form import LowonganForm
 
 url_form_lowongan = '/lowongan/opd/form/'
+url_post_lowongan = url_form_lowongan + "post/"
+list_berkas = ["Kartu Keluarga"]
 mock_date = datetime.date(2012, 12, 12)
 mock_date2 = datetime.date(2011, 11, 11)
 
@@ -15,16 +18,17 @@ class LowonganFormTest(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = Account.objects.create_user('test_user', password='test_user')
+        self.user = Account.objects.create_user('test_user',
+                                                password='test_user')
         self.client.login(username='test_user', password='test_user')
 
         self.lowongan_obj = Lowongan.objects.create(
             judul='judul1',
             kategori='kat1',
             kuota_peserta=10,
-            waktu_awal_magang = mock_date,
-            waktu_akhir_magang = mock_date,
-            batas_akhir_pendaftaran = mock_date,
+            waktu_awal_magang=mock_date,
+            waktu_akhir_magang=mock_date,
+            batas_akhir_pendaftaran=mock_date,
             berkas_persyaratan=['Kartu Keluarga'],
             deskripsi='deskripsi1',
             requirement='requirement1',
@@ -40,6 +44,17 @@ class LowonganFormTest(TestCase):
         Account.objects.filter(pk=self.user.id).update(is_opd=False)
         response = self.client.get(url_form_lowongan)
         self.assertEqual(response.status_code, 302)
+    
+    def test_form_init_form_to_create_choice_update_form(self):
+        choice = (
+                ('Kartu Keluarga', 'Kartu Keluarga'),
+                ('Kartu Tanda Penduduk', 'Kartu Tanda Penduduk'),
+                ('Surat Izin Sekolah', 'Surat Izin Sekolah'),
+            )
+        mock_id_lowongan = self.lowongan_obj.id
+        form = LowonganForm(instance=self.lowongan_obj, id=mock_id_lowongan)
+        self.assertNotEqual(form.choice, choice)
+        self.assertEqual(form.id_lowongan, mock_id_lowongan)
 
     def test_form_lowongan_using_form_lowongan_function(self):
         function_used = resolve(url_form_lowongan)
@@ -52,12 +67,12 @@ class LowonganFormTest(TestCase):
   
     def test_post_form_lowongan_failed_and_redirect(self):
         Account.objects.filter(pk=self.user.id).update(is_opd=True)
-        response = self.client.post(url_form_lowongan+"post/")
+        response = self.client.post(url_post_lowongan)
         self.assertEqual(response.status_code, 302)
     
     def test_post_form_lowongan_url_for_non_opd(self):
         Account.objects.filter(pk=self.user.id).update(is_opd=False)
-        response = self.client.get(url_form_lowongan+"post/")
+        response = self.client.get(url_post_lowongan)
         self.assertEqual(response.status_code, 302)
 
     def test_post_form_lowongan_url_for_opd_and_form_is_valid(self):
@@ -69,14 +84,14 @@ class LowonganFormTest(TestCase):
             "waktu_awal_magang" : mock_date,
             "waktu_akhir_magang" : mock_date,
             "batas_akhir_pendaftaran" : mock_date,
-            "berkas_persyaratan" :['Kartu Keluarga'],
+            "berkas_persyaratan" :list_berkas,
             "deskripsi" :'deskripsi1',
             "requirement" :'requirement1',
             "opd_foreign_key_id" :self.user.id
-        }    
-        response = self.client.post(url_form_lowongan+"post/", data_form_lowongan)
+        }
+        response = self.client.post(url_post_lowongan, data_form_lowongan)
         self.assertTrue(Lowongan.objects.filter(judul="judul1").exists())
-        self.assertEqual(response.status_code, 302) 
+        self.assertEqual(response.status_code, 302)
 
     def test_update_form_lowongan_url_for_non_opd(self):
         Account.objects.filter(pk=self.user.id).update(is_opd=False)
@@ -88,7 +103,7 @@ class LowonganFormTest(TestCase):
         id_lowongan = str(self.lowongan_obj.id)
         response = self.client.get(url_form_lowongan+"edit/"+id_lowongan+"/")
         self.assertEqual(response.status_code, 200)
-    
+
     def test_update_form_lowongan_url_for_opd_and_form_is_valid(self):
         Account.objects.filter(pk=self.user.id).update(is_opd=True)
         id_lowongan = self.lowongan_obj.id
@@ -100,13 +115,13 @@ class LowonganFormTest(TestCase):
             "waktu_awal_magang" : mock_date,
             "waktu_akhir_magang" : mock_date,
             "batas_akhir_pendaftaran" : mock_date,
-            "berkas_persyaratan" :['Kartu Keluarga'],
+            "berkas_persyaratan" :list_berkas,
             "deskripsi" :'deskripsi1',
             "requirement" :'requirement1',
             "opd_foreign_key_id" :self.user.id
         }   
         response = self.client.post(url_form_lowongan+"edit/"+str(id_lowongan)+"/", data_form_lowongan)
-        self.assertTrue(Lowongan.objects.filter(judul="IniUpdate").exists()) 
+        self.assertTrue(Lowongan.objects.filter(judul="IniUpdate").exists())
         self.assertEqual(response.status_code, 302)
 
 class LowonganModelTest(TestCase):
@@ -121,28 +136,28 @@ class LowonganModelTest(TestCase):
         
         self.lowongan1 = Lowongan.objects.create(
             judul='judul1',
-             kategori='kat1',
-             kuota_peserta=10,
-             waktu_awal_magang=mock_date,
-             waktu_akhir_magang=mock_date,
-             batas_akhir_pendaftaran=mock_date,
-             berkas_persyaratan=['Kartu Keluarga'],
-             deskripsi='deskripsi1',
-             requirement='requirement1',
-             opd_foreign_key_id=self.opd1.id
+            kategori='kat1',
+            kuota_peserta=10,
+            waktu_awal_magang=mock_date,
+            waktu_akhir_magang=mock_date,
+            batas_akhir_pendaftaran=mock_date,
+            berkas_persyaratan=list_berkas,
+            deskripsi='deskripsi1',
+            requirement='requirement1',
+            opd_foreign_key_id=self.opd1.id
         )
 
         self.lowongan2 = Lowongan.objects.create(
             judul='judul2',
-             kategori='kat2',
-             kuota_peserta=20,
-             waktu_awal_magang=mock_date2,
-             waktu_akhir_magang=mock_date2,
-             batas_akhir_pendaftaran=mock_date2,
-             berkas_persyaratan=['Surat Izin Sekolah'],
-             deskripsi='deskripsi2',
-             requirement='requirement2',
-             opd_foreign_key_id=self.opd1.id
+            kategori='kat2',
+            kuota_peserta=20,
+            waktu_awal_magang=mock_date2,
+            waktu_akhir_magang=mock_date2,
+            batas_akhir_pendaftaran=mock_date2,
+            berkas_persyaratan=['Surat Izin Sekolah'],
+            deskripsi='deskripsi2',
+            requirement='requirement2',
+            opd_foreign_key_id=self.opd1.id
         )
 
     def test_object_opd_dibuat(self):
@@ -166,7 +181,7 @@ class LowonganModelTest(TestCase):
         self.assertEqual(self.lowongan1.kuota_peserta, 10)
 
     def test_berkas_persyaratan_is_berkas_persyaratan1(self):
-        self.assertEqual(self.lowongan1.berkas_persyaratan, ["Kartu Keluarga"])
+        self.assertEqual(self.lowongan1.berkas_persyaratan, list_berkas)
 
     def test_deskripsi_is_deskripsi1(self):
         self.assertEqual(self.lowongan1.deskripsi, "deskripsi1")
@@ -182,7 +197,7 @@ class LowonganModelTest(TestCase):
 
     def test_waktu_awal_magang_is_2012_12_12(self):
         self.assertEqual(self.lowongan1.waktu_awal_magang, mock_date)
-    
+
     def test_waktu_akhir_magang_is_2012_12_12(self):
         self.assertEqual(self.lowongan1.waktu_akhir_magang, mock_date)
 
@@ -197,29 +212,35 @@ class LowonganModelTest(TestCase):
 
     def test_kategori_lowongan1_and_lowongan2_is_not_equal(self):
         self.assertNotEqual(self.lowongan1.kategori, self.lowongan2.kategori)
-        
+
     def test_kuota_peserta_lowongan1_and_lowongan2_is_not_equal(self):
-        self.assertNotEqual(self.lowongan1.kuota_peserta, self.lowongan2.kuota_peserta)
+        self.assertNotEqual(self.lowongan1.kuota_peserta,
+                            self.lowongan2.kuota_peserta)
 
     def test_waktu_awal_magang_lowongan1_and_lowongan2_is_not_equal(self):
-        self.assertNotEqual(self.lowongan1.waktu_awal_magang, self.lowongan2.waktu_awal_magang)
+        self.assertNotEqual(self.lowongan1.waktu_awal_magang,
+                            self.lowongan2.waktu_awal_magang)
 
     def test_waktu_akhir_magang_lowongan1_and_lowongan2_is_not_equal(self):
-        self.assertNotEqual(self.lowongan1.waktu_akhir_magang, self.lowongan2.waktu_akhir_magang)
+        self.assertNotEqual(self.lowongan1.waktu_akhir_magang,
+                            self.lowongan2.waktu_akhir_magang)
 
     def test_batas_akhir_pendaftaran_lowongan1_and_lowongan2_is_not_equal(self):
-        self.assertNotEqual(self.lowongan1.batas_akhir_pendaftaran, self.lowongan2.batas_akhir_pendaftaran)
+        self.assertNotEqual(self.lowongan1.batas_akhir_pendaftaran,
+                            self.lowongan2.batas_akhir_pendaftaran)
 
     def test_berkas_persyaratan_lowongan1_and_lowongan2_is_not_equal(self):
-        self.assertNotEqual(self.lowongan1.berkas_persyaratan, self.lowongan2.berkas_persyaratan)
+        self.assertNotEqual(self.lowongan1.berkas_persyaratan,
+                            self.lowongan2.berkas_persyaratan)
 
     def test_deskripsi_lowongan1_and_lowongan2_is_not_equal(self):
-        self.assertNotEqual(self.lowongan1.deskripsi, self.lowongan2.deskripsi)
+        self.assertNotEqual(self.lowongan1.deskripsi,
+                            self.lowongan2.deskripsi)
 
     def test_requirement_lowongan1_and_lowongan2_is_not_equal(self):
-        self.assertNotEqual(self.lowongan1.requirement, self.lowongan2.requirement)
+        self.assertNotEqual(self.lowongan1.requirement,
+                            self.lowongan2.requirement)
 
-    
 class AppsTest(TestCase):
     def test_apps(self):
         self.assertEqual(LowonganConfig.name, 'lowongan')
