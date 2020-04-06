68e4fd873dd38562d6db251937b5df35f420fbba
Azhar Rais <dev@azharaiz.com>
2020-04-06 12:46:48 +0700

    [GREEN] Implement Pelamar login functionality

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from . import views
+from .user_login_form import UserAuthenticationForm
 
 urlpatterns = [
     path('register', views.user_register, name='user_register'),
+    path(
+        'login/',
+        LoginView.as_view(
+            template_name='user_login.html',
+            form_class=UserAuthenticationForm,),
+        name='user_login'),
     path('verification/404', views.user_verification_not_found,
          name="user_verification_not_found"),
     path('verification/<str:token>', views.user_verification,
