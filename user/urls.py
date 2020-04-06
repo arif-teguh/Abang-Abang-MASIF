b6ad64e7cd4dbc4ba8e1f3968c13b7ab9995e083
Azhar Rais <dev@azharaiz.com>
2020-04-06 12:36:27 +0700

    [GREEN] Implement Pelamar verification

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('register', views.user_register, name='user_register'),
+    path('verification/404', views.user_verification_not_found,
+         name="user_verification_not_found"),
+    path('verification/<str:token>', views.user_verification,
+         name="user_verification"),
+    path('verification/', views.user_verification_redirect,
+         name="user_verification_redirect"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
+from account.models import Account, PelamarProfile
 from user.models import UserVerificationList
 from .user_registration_form import UserRegistrationForm
 from .token import generate_user_token
@@ -29,3 +31,33 @@ def user_register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'user/user_register.html', {'form': form})
+
+def user_verification(request, token):
+    try:
+        user_from_verification_list = UserVerificationList.objects.get(
+            secret=token)
+        user_name = user_from_verification_list.name
+        email = user_from_verification_list.email
+        phone = user_from_verification_list.phone
+        password = user_from_verification_list.password
+    except:
+        return redirect('/user/verification/404')
+    new_user = Account.objects.create_user(email, password)
+    new_user.name = user_name
+    new_user.phone = phone
+    new_user.is_user = True
+    new_user.save()
+    create_user = PelamarProfile(user=new_user, unique_pelamar_attribute='user')
+    create_user.save()
+    user_from_verification_list.delete()
+    messages.success(request, 'User Verified')
+    return redirect("/user/login")
+
+def user_verification_redirect(request):
+    return redirect("/")
+
+def user_verification_not_found(request):
+    return render(
+        request,
+        'user/user_verification_404.html'
+    )
