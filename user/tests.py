c3a09eb21cbc6c4629c66f7c98356579053cb900
Azhar Rais <dev@azharaiz.com>
2020-04-06 12:35:42 +0700

    [RED] Create Pelamar verification tests

--- user/tests.py
+++ user/tests.py
@@ -24,7 +24,7 @@ class PelamarRegistrationTest(TestCase):
         # Clean up run after every test method.
         pass
 
-    def test_page_title_register_opd_page(self):
+    def test_page_title_register_user_page(self):
         response = views.user_register(self.request)
         html_response = response.content.decode('utf8')
         self.assertIn('<title>Daftar Pelamar</title>', html_response)
@@ -53,3 +53,42 @@ class PelamarRegistrationTest(TestCase):
              'confirm_password': self.password})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(UserVerificationList.objects.count(), user_count+1)
+
+class PelamarValidationTest(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        self.client = Client()
+        self.request = HttpRequest()
+        self.email = "a@a.com"
+        self.password = "zxasqw12"
+        self.user_name = "abc"
+        self.phone = 1234
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_verification_redirect(self):
+        response = self.client.get('/user/verification', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_verification_not_found(self):
+        with self.assertTemplateUsed('user/user_verification_404.html'):
+            response = self.client.get('/user/verification/404')
+            self.assertEqual(response.status_code, 200)
+
+    def test_create_user_to_verification_list(self):
+        secret = generate_user_token()
+        new_user = UserVerificationList(name=self.user_name,
+                                        email=self.email,
+                                        phone=self.phone,
+                                        password=self.password,
+                                        secret=secret)
+        new_user.save()
+        response = self.client.get('/user/verification/'+secret)
+        self.assertEqual(response.status_code, 302)
+
+    def test_open_verification_code_not_exist(self):
+        secret = "abcdefakfjalk"
+        response = self.client.get('/user/verification/'+secret)
+        self.assertEqual(response.status_code, 302) 
