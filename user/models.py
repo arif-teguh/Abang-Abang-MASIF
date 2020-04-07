1f7a5740645e365e292e56e8b255fcd0acdb3700
Azhar Rais <dev@azharaiz.com>
2020-04-07 17:12:00 +0700

    [REFACTOR] Change phone number field from Integer to Char

--- user/models.py
+++ user/models.py
@@ -5,7 +5,7 @@ class UserVerificationList(models.Model):
     secret = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
     creation_date = models.DateTimeField(
         verbose_name='creation date', auto_now_add=True)
