ce770c108a8c4fbd6a68f95cf26969813b000122
Azhar Rais <dev@azharaiz.com>
2020-04-17 21:39:46 +0700

    [REFACTOR] Fix code smells from opd module

--- opd/views.py
+++ opd/views.py
@@ -1,54 +1,52 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth.hashers import check_password
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 from lowongan.models import Lowongan
 from admin.models import OpdVerificationList
-from account.models import Account, OpdProfile  , UserProfile
+from account.models import Account, OpdProfile
 from .opd_confirmation_form import OpdConfirmationForm
+
+URL_OPD_LOGIN = '/opd/login/'
 
 def opd_login(request):
     return render(request, 'opd_login.html')
 
 def opd_list_pendaftar(request, id_lowongan):
     if request.user.is_authenticated and request.user.is_opd:
-        if(cek_id_lowongan_dan_opd(request, id_lowongan)):
-            lowongan = Lowongan.objects.get(id = id_lowongan)
+        if cek_id_lowongan_dan_opd(request, id_lowongan):
+            lowongan = Lowongan.objects.get(id=id_lowongan)
             list_pelamar = lowongan.list_pendaftar_key.all()
             jumlah = list_pelamar.count()
-            return render(request,'opd_list_pendaftar.html',
-            {'lowongan': lowongan ,
-            'list_pelamar' : list_pelamar,
-            'jumlah' : jumlah,
-               } )
-        else :
-            return redirect('/opd/')
-    else:
-        return redirect('/opd/login/')
-  
+            return render(request, 'opd_list_pendaftar.html',
+                          {
+                              'lowongan': lowongan,
+                              'list_pelamar' : list_pelamar,
+                              'jumlah' : jumlah,
+                          })
+        return redirect('/opd/')
+    return redirect(URL_OPD_LOGIN)
+
 def cek_id_lowongan_dan_opd(request, id_lowongan):
-    lowongan = Lowongan.objects.get(id = id_lowongan)
-    if(lowongan.opd_foreign_key_id == request.user.id ):
+    lowongan = Lowongan.objects.get(id=id_lowongan)
+    if lowongan.opd_foreign_key_id == request.user.id:
         return True
     else:
         return False
 
 def opd_lowongan(request):
     if request.user.is_authenticated and request.user.is_opd:
-        list_lowongan = Lowongan.objects.filter(opd_foreign_key = request.user.id)
-        
-        return render(request,'opd_lowongan.html', {'list_lowongan': list_lowongan})
-    else:
-        return redirect('/opd/login/')
+        list_lowongan = Lowongan.objects.filter(opd_foreign_key=request.user.id)
+
+        return render(request, 'opd_lowongan.html',
+                      {'list_lowongan': list_lowongan})
+    return redirect(URL_OPD_LOGIN)
 
 
-def opd_detail_lowongan(request,id_lowongan):
+def opd_detail_lowongan(request, id_lowongan):
     if request.user.is_authenticated and request.user.is_opd:
-        list_lowongan = Lowongan.objects.filter(opd_foreign_key = request.user.id)
-        lowongan = Lowongan.objects.get(id = id_lowongan)
-        return render(request,'opd_detail_lowongan.html' , {'lowongan': lowongan})
-    else:
-        return redirect('/opd/login/')
+        lowongan = Lowongan.objects.get(id=id_lowongan)
+        return render(request, 'opd_detail_lowongan.html',
+                      {'lowongan': lowongan})
+    return redirect(URL_OPD_LOGIN)
 
 def opd_verification(request, token):
     try:
@@ -73,7 +71,7 @@ def opd_verification(request, token):
             create_opd = OpdProfile(user=new_user, unique_opd_attribute="opd")
             create_opd.save()
             opd_from_verification_list.delete()
-            return redirect("/opd/login")
+            return redirect(URL_OPD_LOGIN)
     else:
         form = OpdConfirmationForm()
     return render(
