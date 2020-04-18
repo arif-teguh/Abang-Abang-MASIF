ef166a173d74b7c3216991654d119b9134c272a4
khrisnaa3 <stefanuskhrisnaproject@gmail.com>
2020-04-19 00:15:57 +0700

    [GREEN] Implement unit test for sorting and searching

--- cari_lowongan/urls.py
+++ cari_lowongan/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('detail-lowongan/', include('detail_lowongan.urls')),
     path('', views.cari_lowongan),
+    path('detail-lowongan/', include('detail_lowongan.urls')),
+    path('sorting/waktu-magang/<str:param>', views.sort_by_waktu_magang),
+    path('sorting/batas-akhir/<str:param>', views.sort_by_batas_akhir),
+    path('searching/<str:param>', views.search_by_judul),
 ]
