5db94d46232483a06f80a1053c78a9cb625cc47d
Azhar Rais <dev@azharaiz.com>
2020-04-19 17:24:15 +0700

    [GREEN] Implement logout

--- landing/urls.py
+++ landing/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.landing),
+    path('logout/', views.landing_logout, name="logout")
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import logout
 
-
-# Create your views here.
 
 def landing(request):
     return render(request, 'landing_page.html')
+
+def landing_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
