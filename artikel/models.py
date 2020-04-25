3900c65bdd0e297aec581fafe90a25ed9ef25972
feril68 <muhammad.feril@ui.ac.id>
2020-04-25 11:19:34 +0700

    [GREEN] menambahkan field db foto di Artikel

--- artikel/models.py
+++ artikel/models.py
@@ -4,3 +4,4 @@ class Artikel(models.Model):
     judul = models.CharField(max_length=100)
     deskripsi = models.TextField()
     waktu_dibuat = models.DateTimeField(auto_now=True)
+    foto_artikel = models.FileField(null=True, blank=True)
