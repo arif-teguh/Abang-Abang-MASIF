0a3f55898de69423cb012ff5ed85306de5f489e5
feril68 <muhammad.feril@ui.ac.id>
2020-04-29 09:19:39 +0700

    [RED] membuat test untuk fitur membuat dan mengedit form artikel

--- artikel/tests.py
+++ artikel/tests.py
@@ -1,10 +1,15 @@
 import tempfile
 import shutil
-from django.test import TestCase, override_settings
+from django.test import TestCase, override_settings, Client
 from django.core.files.uploadedfile import SimpleUploadedFile
+from account.models import Account
 from .models import Artikel
 
 MEDIA_ROOT = tempfile.mkdtemp()
+url_form_artikel = "/artikel/form/"
+url_post_form_artikel = url_form_artikel+"post/"
+url_update_form_artikel = url_form_artikel+"edit/"
+
 @override_settings(MEDIA_ROOT=MEDIA_ROOT)
 class ArtikelModelTest(TestCase):
     def setUp(self):
@@ -57,3 +62,91 @@ class ArtikelModelTest(TestCase):
 
     def test_waktu_dibuat_not_none(self):
         self.assertIsNotNone(self.artikel1.waktu_dibuat)
+
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
+class ArtikelFormTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = Account.objects.create_user('test_user',
+                                                password='test_user')
+        self.client.login(username='test_user', password='test_user')
+        self.test_file_jpg_a = SimpleUploadedFile("fotoa.jpg", b"file_content")
+        self.test_file_jpg_b = SimpleUploadedFile("fotob.jpg", b"file_content")
+        self.artikel_obj = Artikel.objects.create(
+            judul="judul1",
+            deskripsi="desk1",
+            foto_artikel=self.test_file_jpg_a
+        )
+        self.artikel_obj.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+        super().tearDownClass()
+
+    def test_form_artikel_url_exist(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=True)
+        response = self.client.get(url_form_artikel)
+        self.assertEqual(response.status_code, 200)
+
+    def test_form_arikel_url_for_non_opd(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=False)
+        response = self.client.get(url_form_artikel)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_form_artikel_open_raw_url_redirect(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=True)
+        response = self.client.get(url_post_form_artikel)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_form_lowongan_url_for_non_opd(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=False)
+        response = self.client.get(url_post_form_artikel)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_form_artikel_url_for_admin_and_form_is_valid(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=True)
+        data_artikel = {
+            "judul":"judul_admin",
+            "deskripsi":"desk1",
+            "foto_artikel":self.test_file_jpg_a
+        }
+        response = self.client.post(url_post_form_artikel, data_artikel)
+        self.assertTrue(Artikel.objects.filter(judul="judul_admin").exists())
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_form_artikel_url_not_admin_and_form_is_valid(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=False)
+        data_artikel = {
+            "judul":"judul_admin",
+            "deskripsi":"desk1",
+            "foto_artikel":self.test_file_jpg_a
+        }
+        response = self.client.post(url_post_form_artikel, data_artikel)
+        self.assertFalse(Artikel.objects.filter(judul="judul_admin").exists())
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_form_artikel_url_for_non_opd(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=False)
+        response = self.client.get(url_update_form_artikel+"111/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_form_artikel_url_for_admin_and_form_not_is_valid(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=True)
+        id_artikel = str(self.artikel_obj.id)
+        response = self.client.get(url_update_form_artikel+id_artikel+"/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_form_artikel_url_for_admin_and_form_is_valid(self):
+        Account.objects.filter(pk=self.user.id).update(is_admin=True)
+        id_artikel = self.artikel_obj.id
+        data_artikel = {
+            "id":id_artikel,
+            "judul":"judul_update",
+            "deskripsi":"desk1",
+            "foto_artikel":self.test_file_jpg_b
+        }
+        url_update = url_update_form_artikel+str(id_artikel)+"/"
+        response = self.client.post(url_update, data_artikel)
+        self.assertTrue(Artikel.objects.filter(judul="judul_update").exists())
+        self.assertEqual(response.status_code, 302)
