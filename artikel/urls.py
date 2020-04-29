024be24e2b463cc774347c6dec278e9821938ea6
feril68 <muhammad.feril@ui.ac.id>
2020-04-30 03:54:34 +0700

    [GREEN] implementasi pembuatan dan mengedit form artikel

--- artikel/urls.py
+++ artikel/urls.py
@@ -2,4 +2,16 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('form/',
+         views.show_form_artikel,
+         name="show_form_artikel"),
+
+    path('form/post/',
+         views.post_form_artikel,
+         name="post_form_artikel"),
+
+    path('form/edit/<int:id_artikel>/',
+         views.update_form_artikel,
+         name="update_form_artikel"),
+]
